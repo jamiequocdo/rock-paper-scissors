34a5239adafa60e2b6e97342b54893564a3687a8
Jamie <jamiedofitness@gmail.com>
2023-09-08 06:45:44 -0700

    Add eventListeners to Rock, Paper, Scissors Button Game playable in console.log Remove "Incorrect Input" line in playRound() function

--- script.js
+++ script.js
@@ -10,44 +10,68 @@ function getComputerChoice() {
     }
 }
 
-const computerSelection = getComputerChoice();
+let computerSelection = getComputerChoice();
 let playerSelection = "";
 let computerScore = 0;
 let playerScore = 0;
 
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+
+rock.addEventListener("click", () => {
+    playerSelection = "rock";
+    computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+});
+
+paper.addEventListener("click", () => {
+    playerSelection = "paper";
+    computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+})
+
+scissors.addEventListener("click", () => {
+    playerSelection = "scissors";
+    computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+})
 //Get playerSelection to populate with a string according to the button the player presses
     //The player clicks on rock, paper, or scissors
         //if the player clicks on rock, it will populate the playerSelection as "rock"
-            //An eventListener with "click" needs to be applied to the button
-                //For the button to have an event listener, it needs to be called with document.getElementById("rock")
+            //*An eventListener with "click" needs to be applied to the button
+            //
+
+                //*For the button to have an event listener, it needs to be called with document.getElementById("")
+                //Create variable rock add value of button id="rock"
+                //create variable paper and add value of paper button
+                //create variable of scissors and add value of scissors button
+
+
+
 
 /*Create a function that plays one Round of Rock, Paper, Scissors*/
 function playRound (playerSelection, computerSelection) {
-    playerSelection = prompt("Will you choose rock, paper, or scissors?", "").toLowerCase();
 
-/*Computer Wins*/
-if ((playerSelection === "rock" && computerSelection === "paper") ||
-    (playerSelection === "paper" && computerSelection === "scissors") ||
-    (playerSelection === "scissors" && computerSelection === "rock")) {
-        alert(`Computer Wins, ${computerSelection} beats ${playerSelection}`);
-        computerScore++;
-        alert(`Player score is ${playerScore}. Computer score is ${computerScore}.`)
-/*Player Wins*/
+    /*Computer Wins*/
+    if ((playerSelection === "rock" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "rock")) {
+            console.log(`Computer Wins, ${computerSelection} beats ${playerSelection}`);
+            computerScore++;
+            console.log(`Player score is ${playerScore}. Computer score is ${computerScore}.`)
+    /*Player Wins*/
     } else if ( (playerSelection === "paper" && computerSelection === "rock") ||
             (playerSelection === "scissors" && computerSelection === "paper") ||
             (playerSelection === "rock" && computerSelection === "scissors")) {
-                alert(`Player Wins, ${playerSelection} beats ${computerSelection}`);
+                console.log(`Player Wins, ${playerSelection} beats ${computerSelection}`);
                 playerScore++;
-                alert(`Player score is ${playerScore}. Computer score is ${computerScore}.`)
-/*Tie Game*/
+                console.log(`Player score is ${playerScore}. Computer score is ${computerScore}.`)
+    /*Tie Game*/
     } else if ( (playerSelection === "rock" && computerSelection === "rock") ||
                 (playerSelection === "paper" && computerSelection === "paper") ||
                 (playerSelection === "scissors" && computerSelection === "scissors")) {
-                    alert(`Tie Game, both chose ${playerSelection}`);
-                    alert(`Player score is ${playerScore}. Computer score is ${computerScore}.`)
-    } else {
-        alert("Incorrect input");
-    }
+                    console.log(`Tie Game, both chose ${playerSelection}`);
+                    console.log(`Player score is ${playerScore}. Computer score is ${computerScore}.`)
+    } 
 }
-
-playRound(playerSelection, computerSelection);
